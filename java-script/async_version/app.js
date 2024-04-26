bf3b9973a44a331b5801e20352e68d8651d9f240
Mikhail Sokolov <mi.sokolov@innopolis.ru>
2024-04-26 21:09:11 +0300

    Refactoring

--- java-script/async_version/app.js
+++ java-script/async_version/app.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 9001;
 http.createServer((request, response) => {
   const parsedUrl = require('url').parse(request.url, true);
 
-  // Используем console.time для начала отсчета времени
-  console.time('Request Processing Time');
+  const startTime = Date.now();
 
   if (parsedUrl.pathname === '/getSubsets') {
     const inputData = JSON.parse(parsedUrl.query.data);
@@ -15,12 +14,13 @@ http.createServer((request, response) => {
 
     const SubsetProcessor = new Processor(parsedUrl.query.sum, inputData);
     SubsetProcessor.on('end', (matchCount) => {
-      // Используем console.timeEnd для окончания отсчета времени
-      console.timeEnd('Request Processing Time');
+      const endTime = Date.now();
+      const deltaTime = endTime - startTime;
+
       const responseData = {
         Service: "JavaScript Async Version",
         Result: matchCount,
-        Time: `${(process.hrtime()[0] * 1000 + process.hrtime()[1] / 1000000).toFixed(3)}ms`
+        Time: `${deltaTime.toFixed(3)}ms`
       };
       response.end(JSON.stringify(responseData) + '\n');
     });
@@ -29,8 +29,5 @@ http.createServer((request, response) => {
   } else {
     response.writeHead(200);
     response.end("Successful!\n");
-
-    // В случае других эндпоинтов также используем console.timeEnd
-    console.timeEnd('Request Processing Time');
   }
 }).listen(port, () => console.log('Started server on port ' + port));
--- java-script/sync_version/app.js
+++ java-script/sync_version/app.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 9000;
 http.createServer((request, response) => {
   const parsedUrl = require('url').parse(request.url, true);
 
-  // Используем console.time для начала отсчета времени
-  console.time('Request Processing Time');
+  const startTime = Date.now();
 
   if (parsedUrl.pathname === '/getSubsets') {
     const inputData = JSON.parse(parsedUrl.query.data);
@@ -15,22 +14,19 @@ http.createServer((request, response) => {
 
     const SubsetProcessor = new Processor(parsedUrl.query.sum, inputData);
     SubsetProcessor.on('end', (matchCount) => {
-      // Используем console.timeEnd для окончания отсчета времени
-      console.timeEnd('Request Processing Time');
+      const endTime = Date.now();
+      const deltaTime = endTime - startTime;
       const responseData = {
         Service: "JavaScript Sync Version",
         Result: matchCount,
-        Time: `${(process.hrtime()[0] * 1000 + process.hrtime()[1] / 1000000).toFixed(3)}ms`
+        Time: `${deltaTime.toFixed(3)}ms`
       };
       response.end(JSON.stringify(responseData) + '\n');
     });
-    
+
     SubsetProcessor.initiate();
   } else {
     response.writeHead(200);
     response.end("Successful!\n");
-
-    // В случае других эндпоинтов также используем console.timeEnd
-    console.timeEnd('Request Processing Time');
   }
 }).listen(port, () => console.log('Started server on port ' + port));
