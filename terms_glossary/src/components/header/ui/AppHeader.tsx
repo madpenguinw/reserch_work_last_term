94438e4eae4ebc18682700a9b9866f21b0db1b3d
Mikhail Sokolov <mi.sokolov@innopolis.ru>
2024-04-26 15:47:16 +0300

    Fixing terms_glossary

--- terms_glossary/src/components/header/ui/AppHeader.tsx
+++ terms_glossary/src/components/header/ui/AppHeader.tsx
@@ -7,7 +7,7 @@ const AppHeader: FC = () => {
     <header className="fixed left-0 right-0 top-0 z-[50] flex h-30 items-center justify-center bg-white px-20 py-10 shadow-md lg:px-16">
       <div className="flex items-center gap-x-10">
         <NavLink
-          to="/"
+          to="/terms_glossary/"
           className={({ isActive }) =>
             clsx('text-12', { 'text-blue-500': isActive })
           }
@@ -15,7 +15,7 @@ const AppHeader: FC = () => {
           Глоссарий
         </NavLink>
         <NavLink
-          to="/mindmap"
+          to="/terms_glossary/mindmap"
           className={({ isActive }) =>
             clsx('text-12', { 'text-blue-500': isActive })
           }
