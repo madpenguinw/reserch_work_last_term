cb222dd2675b29907b63fdbfdaa9494719439236
Mikhail Sokolov <mi.sokolov@innopolis.ru>
2024-06-17 03:19:55 +0300

    Small improvments

--- python/fastapi_app/scripts/get_versions.py
+++ python/fastapi_app/scripts/get_versions.py
@@ -1,6 +1,7 @@
 import asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
+
 import asyncpg
+from motor.motor_asyncio import AsyncIOMotorClient
 
 # MongoDB configuration
 MONGO_URL = "mongodb://admin:12345678@localhost:3456"
@@ -12,12 +13,12 @@ POSTGRES_URL = "postgresql://admin:12345678@localhost:1234/VKR"
 async def get_mongodb_version():
     mongo_client = AsyncIOMotorClient(MONGO_URL)
     server_info = await mongo_client.server_info()
-    return server_info['version']
+    return server_info["version"]
 
 
 async def get_postgresql_version():
     conn = await asyncpg.connect(POSTGRES_URL)
-    version = await conn.fetchval('SELECT version()')
+    version = await conn.fetchval("SELECT version()")
     await conn.close()
     return version
 
@@ -25,7 +26,7 @@ async def get_postgresql_version():
 async def main():
     mongodb_version = await get_mongodb_version()
     postgresql_version = await get_postgresql_version()
-    
+
     print(f"MongoDB version: {mongodb_version}")
     print(f"PostgreSQL version: {postgresql_version}")
 
